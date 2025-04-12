7a23de76fd29374d4548c1ada3d1b4eedf8f95c0
Nitin <nitinchaube08@gmail.com>
2025-04-12 14:36:48 -0400

    changedd .json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+isProd = true
 const nextConfig = { 
     images: { unoptimized: true }, 
     output: 'export',
+    basePath: isProd ? '/personal-website' : '',
+    assetPrefix: isProd ? '/personal-website/' : '',
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
