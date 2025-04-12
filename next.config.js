6fdd21b19d2f6179e515bff7e938505e3060fd25
Nitin <nitinchaube08@gmail.com>
2025-04-12 15:08:10 -0400

    added yml

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
-isProd = true
 const nextConfig = { 
-    images: { unoptimized: true }, 
-    output: 'export',
-    basePath: isProd ? '/personal-website' : '',
-    assetPrefix: isProd ? '/personal-website/' : '',
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-      }
+  images: { unoptimized: true }, 
+  output: 'export',
+  eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    }
 };
 
 module.exports = nextConfig;
